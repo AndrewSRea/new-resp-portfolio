25bf7627bd9362afde5aa631c953525ead34a3ea
AndrewSRea <andrew.s.rea@gmail.com>
2020-02-11 12:21:39 -0800

    Finished scripts for images carousel

--- js/imagesloaded.pkgd.js
+++ js/imagesloaded.pkgd.js
@@ -273,8 +273,208 @@ ImagesLoaded.prototype.addElementImages = function( elem ) {
     }
 };
 
+var elementNodeTypes = {
+    1: true,
+    9: true,
+    11: true
+};
 
+ImagesLoaded.prototype.addElementBackgroundImages = function( elem ) {
+    var style = getComputedStyle( elem );
+    if ( !style ) {
+        // Firefox returns null if in a hidden form https://bugzil.la/548397
+        return;
+    }
+    // get url inside url("...")
+    var reURL = /url\((['"])?(.*?)\1\)/gi;
+    var matches = reURL.exec( style.backgroundImage );
+    while ( matches !== null ) {
+        var url = matches && matches[2];
+        if ( url ) {
+            this.addBackground( url, elem );
+        }
+        matches = reURL.exec( style.backgroundImage );
+    }
+};
 
+/**
+ * @param {Image} img
+ */
+
+ImagesLoaded.prototype.addImage = function( img ) {
+    var loadingImage = new loadingImage( img );
+    this.images.push( loadingImage );
+};
+
+ImagesLoaded.prototype.check = function() {
+    var _this = this;
+    this.progressedCount = 0;
+    this.hasAnyBroken = false;
+    // complete if no images
+    if ( !this.images.length ) {
+        this.complete();
+        return;
+    }
+
+    function onProgress( image, elem, message ) {
+        // HACK - Chrome triggers event before object properties have changed. #83
+        setTimeout( function() {
+            _this.progressedCount( image, elem, message );
+        });
+    }
+
+    this.images.forEach( function( loadingImage ) {
+        loadingImage.once( 'progress', onProgress );
+        loadingImage.check();
+    });
+};
+
+ImagesLoaded.prototype.progress = function( image, elem, message ) {
+    this.progressedCount++;
+    this.hasAnyBroken = this.hasAnyBroken || !image.isLoaded;
+    // progress event
+    this.emitEvent( 'progress', [ this, image, elem ] );
+    if ( this.jqDeferred && this.jqDeferred.notify ) {
+        this.jqDeferred.notify( this, image );
+    }
+    // check if completed
+    if ( this.progressedCount == this.images.length ) {
+        this.complete();
+    }
+
+    if ( this.options.debug && console ) {
+        console.log( 'progress: ' + message, image, elem );
+    }
+};
+
+ImagesLoaded.prototype.complete = function() {
+    var eventName = this.hasAnyBroken ? 'fail' : 'done';
+    this.isComplete = true;
+    this.emitEvent( eventName, [ this ] );
+    this.emitEvent( 'always', [ this ] );
+    if ( this.jqDeferred ) {
+        var jqMethod = this.hasAnyBroken ? 'reject' : 'resolve';
+        this.jqDeferred[ jqMethod ]( this );
+    }
+};
+
+// -------------------------------------------------------------------- //
+
+function LoadingImage( img ) {
+    this.img = img;
 }
 
-    )
+LoadingImage.prototype = Object.create( EvEmitter.prototype );
+
+LoadingImage.prototype.check = function() {
+    // If complete is true and browser supports natural sizes,
+    // try to check for image status manually.
+    var isComplete = this.getIsImageComplete();
+    if ( isComplete ) {
+        // report based on naturalWidth
+        this.confirm( this.img.naturalWidth !== 0, 'naturalWidth' );
+        return;
+    }
+
+    // If none of the checks above matched, simulate loading on detached element.
+    this.proxyImage = new Image();
+    this.proxyImage.addEventListener( 'load', this );
+    this.proxyImage.addEventListener( 'error', this );
+    // bind to image as well for Firefox. #191
+    this.img.addEventListener( 'load', this );
+    this.img.addEventListener( 'error', this );
+    this.proxyImage.src = this.img.src;
+};
+
+LoadingImage.prototype.getIsImageComplete = function() {
+    return this.img.complete && this.img.naturalWidth !== undefined;
+};
+
+LoadingImage.prototype.confirm = function( isLoaded, message ) {
+    this.isLoaded = isLoaded;
+    this.emitEvent( 'progress', [ this, this.img, message ] );
+};
+
+// ----- events ----- //
+
+// trigger specified handler for event type
+LoadingImage.prototype.handleEvent = function( event ) {
+    var method = 'on' + event.type;
+    if ( this[ method ] ) {
+        this[ method ]( event );
+    }
+};
+
+LoadingImage.prototype.onload = function() {
+    this.confirm( true, 'onload' );
+    this.unbindEvents();
+};
+
+LoadingImage.prototype.onerror = function() {
+    this.confirm( false, 'onerror' );
+    this.unbindEvents();
+};
+
+LoadingImage.prototype.unbindEvents = function() {
+    this.proxyImage.removeEventListener( 'load', this );
+    this.proxyImage.removeEventListener( 'error', this );
+    this.img.removeEventListener( 'load', this );
+    this.img.removeEventListener( 'error', this );
+};
+
+// ------------------------ Background --------------------------- //
+
+function Background( url, element ) {
+    this.url = url;
+    this.element = element;
+    this.img = new Image();
+}
+
+// inherit LoadingImage prototype
+Background.prototype = Object.create( LoadingImage.prototype );
+
+Background.prototype.check = function() {
+    this.img.addEventListener( 'load', this );
+    this.img.addEventListener( 'error', this );
+    this.img.src = this.url;
+    // check if image is already complete
+    var isComplete = this.getIsImageComplete();
+    if ( isComplete ) {
+        this.confirm( this.img.naturalWidth !== 0, 'naturalWidth' );
+        this.unbindEvents();
+    }
+};
+
+Background.prototype.unbindEvents = function() {
+    this.img.removeEventListener( 'load', this );
+    this.img.removeEventListener( 'error', this );
+};
+
+Background.prototype.confirm = function( isLoaded, message ) {
+    this.isLoaded = isLoaded;
+    this.emitEvent( 'progress', [ this, this.element, message ] );
+};
+
+// -------------------------- jQuery ---------------------------- //
+
+ImagesLoaded.makeJQueryPlugin = function( jQuery ) {
+    jQuery = jQuery || window.jQuery;
+    if ( !jQuery ) {
+        return;
+    }
+    // set local variable
+    $ = jQuery;
+    // $().imagesLoaded()
+    $.fn.imagesLoaded = function( options, callback ) {
+        var instance = new ImagesLoaded( this, options, callback );
+        return instance.jqDeferred.promise( $(this) );
+    };
+};
+// try making plugin
+ImagesLoaded.makeJQueryPlugin();
+
+// --------------------------------------------------------------- //
+
+return ImagesLoaded;
+
+});
