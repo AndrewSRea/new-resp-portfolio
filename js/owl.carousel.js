2d1ddaff57eff20b5c18638ec85b0f2c9c25c30d
AndrewSRea <andrew.s.rea@gmail.com>
2020-02-14 17:43:19 -0800

    Adding more CSS rules to the carousel

--- js/owl.carousel.js
+++ js/owl.carousel.js
@@ -1757,7 +1757,7 @@
 /**
  * AutoRefresh Plugin
  * @version 2.3.4
- * @author Artus Kolanski
+ * @author Artus Kolanowski
  * @author David Deutsch
  * @license The MIT License (MIT)
  */
@@ -2603,7 +2603,7 @@
  * Autoplay Plugin
  * @version 2.3.4
  * @author Bartosz Wojciechowski
- * @author Artus Kolanski
+ * @author Artus Kolanowski
  * @author David Deutsch
  * @author Tom De Caluwe
  * @license The MIT License (MIT)
@@ -2788,6 +2788,336 @@
      * Stops the autoplay.
      * @public
      */
+    Autoplay.prototype.stop = function() {
+        if (this._core.is('rotating')) {
+            // Reset the clock.
+            this._time = 0;
+            this._paused = true;
+
+            window.clearTimeout(this._call);
+            this._core.leave('rotating');
+        }
+    };
+
+    /** 
+     * Pauses the autoplay.
+     * @public
+    */
+    Autoplay.prototype.pause = function() {
+        if (this._core.is('rotating') && !this._paused) {
+            // Pause the clock.
+            this._time = this.read();
+            this._paused = true;
+
+            window.clearTimeout(this._call);
+        }
+    };
+
+    /**
+     * Destroys the plugin.
+     */
+    Autoplay.prototype.destroy = function() {
+        var handler, property;
+
+        this.stop();
+
+        for (handler in this._handlers) {
+            this._core.$element.off(handler, this._handlers[handler]);
+        }
+        for (property in Object.getOwnPropertyNames(this)) {
+            typeof this[property] != 'function' && (this[property] = null);
+        }
+    };
+
+    $.fn.owlCarousel.Constructor.Plugins.autoplay = Autoplay;
+
+})(window.Zepto || window.jQuery, window, document);
+
+/**
+ * Navigation Plugin
+ * @version 2.3.4
+ * @author Artus Kolanowski
+ * @author David Deutsch
+ * @license The MIT License (MIT)
+ */
+;(function($, window, document, undefined) {
+    'use strict';
+
+    /**
+     * Creates the navigation plugin.
+     * @class The Navigation Plugin
+     * @param {Owl} carousel - The Owl Carousel.
+     */
+    var Navigation = function(carousel) {
+        /**
+         * Reference to the core.
+         * @protected
+         * @type {Owl}
+         */
+        this._core = carousel;
+
+        /**
+         * Indicates whether the plugin is initialized or not.
+         * @protected
+         * @type {Boolean}
+         */
+        this._initialized = false;
+
+        /**
+         * The current paging indexes.
+         * @protected
+         * @type {Array}
+         */
+        this._pages = [];
+
+        /**
+         * All DOM element of the user interface.
+         * @protected
+         * @type {Object}
+         */
+        this._controls = {};
+
+        /**
+         * Markup for an indicator.
+         * @protected
+         * @type {Array.<String>}
+         */
+        this._templates = [];
+
+        /**
+         * The carousel element.
+         * @type {jQuery}
+         */
+        this.$element = this._core.$element;
+
+        /**
+         * Overridden methods of the carousel.
+         * @protected
+         * @type {Object}
+         */
+        this._overrides = {
+            next: this._core.next,
+            prev: this._core.prev,
+            to: this._core.to
+        };
+
+        /**
+         * All event handlers.
+         * @protected
+         * @type {Object}
+         */
+        this._handlers = {
+            'prepared.owl.carousel': $.proxy(function(e) {
+                if (e.namespace && this._core.settings.dotsData) {
+                    this._templates.push('<div class="' + this._core.settings.dotClass + '">' +
+                        $(e.content).finc('[data-dot').addBack('[data-dot') + '</div>');
+                }
+            }, this),
+            'added.owl.carousel': $.proxy(function(e) {
+                if (e.namespace && this._core.settings.dotsData) {
+                    this._templates.splice(e.position, 0, this._templates.pop());
+                }
+            }, this),
+            'remove.owl.carousel': $.proxy(function(e) {
+                if (e.namespace && this._core.settings.dotsData) {
+                    this._template.splice(e.position, 1);
+                }
+            }, this),
+            'changed.owl.carousel': $.proxy(function(e) {
+                if (e.namespace && e.property.name == 'position') {
+                    this.draw();
+                }
+            }, this),
+            'initialized.owl.carousel': $.proxy(function(e) {
+                if (e.namespace && !this._initialized) {
+                    this._core.trigger('initialize', null, 'navigation');
+                    this.initialize();
+                    this.update();
+                    this.draw();
+                    this._initialized = true;
+                    this._core.trigger('initialized', null, 'navigation');
+                }
+            }, this),
+            'refreshed.owl.carousel': $.proxy(function(e) {
+                if (e.namespace && this._initialized) {
+                    this._core.trigger('refresh', null, 'navigation');
+                    this.update();
+                    this.draw();
+                    this._core.trigger('refreshed', null, 'navigation');
+                }
+            }, this)
+        };
+
+        // set default options
+        this._core.options = $.extend({}, Navigation.Defaults, this._core.options);
+
+        // register event handlers
+        this.$element.on(this._handlers);
+    };
+
+    /**
+     * Default options.
+     * @public
+     * @todo Rename 'slideBy' to 'navBy'
+     */
+    Navigation.Defaults = {
+        nav: false,
+        navText: [
+            '<span aria-label="' + 'Previous' + '">&#x2039;</span>',
+            '<span aria-label=' + 'Next' + '">&#x203a;</span>'
+        ],
+        navSpeed: false,
+        navElement: 'button type="button" role="presentation"',
+        navContainer: false,
+        navContainerClass: 'owl-nav',
+        navClass: [
+            'owl-prev',
+            'owl-next'
+        ],
+        slideBy: 1,
+        dotClass: 'owl-dot',
+        dotsClass: 'owl-dots',
+        dots: true,
+        dotsEach: false,
+        dotsData: false,
+        dotsSpeed: false,
+        dotsContainer: false
+    };
+
+    /**
+     * Initializes the layout of the plugin and extends the carousel.
+     * @protected
+     */
+    Navigation.prototype.initialize = function() {
+        var override,
+            settings = this._core.settings;
+
+        // create DOM structure for relative navigation
+        this._controls.$relative = (settings.navContainer ? $(settings.navContainer)
+            : $('<div>').addClass(settings.navContainerClass).appendTo(this.$element)).addClass('disabled');
+
+        this._controls.$previous = $('<' + settings.navElement + '>')
+            .addClass(settings.navClass[0])
+            .html(settings.navText[0])
+            .prependTo(this._controls.$relative)
+            .on('click', $.proxy(function(e) {
+                this.prev(settings.navSpeed);
+            }, this));
+        this._controls.$next = $('<' + settings.navElement + '>')
+            .addClass(settings.navClass[1])
+            .html(settings.navText[1])
+            .appendTo(this._controls.$relative)
+            .on('click', $.proxy(function(e) {
+                this.next(settings.navSpeed);
+            }, this));
+
+        // create DOM structure for absolute navigation
+        if (!settings.dotsData) {
+            this._templates = [ $('<button role="button">')
+            .addClass(settings.dotClass)
+            .append($('<span>'))
+            .prop('outerHTML') ];
+        }
+
+        this._controls.$absolute = (settings.dotsContainer ? $(settings.dotsContainer)
+            : $('<div>').addClass(setings.dotsClass).appendTo(this.$element)).addClass('disabled');
+
+        this._controls.$absolute.on('click', 'button', $.proxy(function(e) {
+            var index = $(e.target).parent().is(this._controls.$absolute)
+                ? $(e.target).index() : $(e.target).parent().index();
+
+            e.preventDefault();
+
+            this.to(index, settings.dotsSpeed);
+        }, this));
+
+        /*
+        block
+        block
+        block
+        block
+        block
+        block
+        block
+        block
+        block
+        block
+        */
+
+        // override public methods of the carousel
+        for (override in this._overrides) {
+            this.core[override] = $.proxy(this[override], this);
+        }
+    };
+
+    /**
+     * Destroys the plugin
+     * @protected
+     */
+    Navigation.prototype.destroy = function() {
+        var handler, control, property, override, settings;
+        settings = this._core.settings;
+
+        for (handler in this._handlers) {
+            this.$element.off(handler, this._handlers[handler]);
+        }
+        for (control in this._controls) {
+            if (control === '$relative' && settings.navContainer) {
+                this._controls[control].html('');
+            } else {
+                this._controls[control].remove();
+            }
+        }
+        for (override in this.overrides) {
+            this._core[override] = this.overrides[override];
+        }
+        for (property in Object.getOwnPropertyNames(this)) {
+            typeof this[property] != 'function' && (this[property] = null);
+        }
+    };
+
+    /**
+     * Updates the interal state.
+     * @protected
+     */
+    Navigation.prototype.update = function() {
+        var i, j, k,
+            lower = this._core.clones().length / 2,
+            upper = lower + this._core.items().length,
+            maximum = this._core.maximum(true),
+            settings = this._core.settings,
+            size = settings.center || settings.autoWidth || settings.dotsData
+                ? 1 : settings.dotsEach || settings.items;
+
+        if (settings.slideBy != 'page') {
+            settings.slideBy = Math.min(settings.slideBy, settings.items);
+        }
+
+        if (settings.dots || settings.slideBy == 'page') {
+            this._pages = [];
+
+            for (i = lower, j = 0, k = 0; i < upper; i++) {
+                if (j >= size || j === 0) {
+                    this._pages.push({
+                        start: Math.min(maximum, i - lower),
+                        end: i - lower + size - 1
+                    });
+                    if (Math.min(maximum, i - lower) === maximum) {
+                        break;
+                    }
+                    j = 0, ++k;
+                }
+                j += this._core.mergers(this._core.relative(i));
+            }
+        }
+    };
+
+    /**
+     * Draws the user interface.
+     * @todo The option 'dotsData' won't work.
+     * @protected
+     */
+
 
 })
 
