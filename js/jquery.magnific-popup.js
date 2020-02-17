069a8fbd65045a00a6efc08b044e01a5680251d1
AndrewSRea <andrew.s.rea@gmail.com>
2020-02-17 15:44:47 -0800

    Addingmore scripts to the magnific-popup

--- js/jquery.magnific-popup.js
+++ js/jquery.magnific-popup.js
@@ -1037,9 +1037,433 @@
                         };
 
                         _mfpTrigger('ParseAjax', temp);
+
+                        mfp.appendContent($(temp.data), AJAX_NS);
+
+                        item.finished = true;
+
+                        _removeAjaxCursor();
+
+                        mfp._setFocus();
+
+                        setTimeout(function() {
+                            mfp.wrap.addClass(READY_CLASS);
+                        }, 16);
+
+                        mfp.updateStatus('ready');
+
+                        _mfpTrigger('AjaxContentAdded');
+                    },
+                    error: function() {
+                        _removeAjaxCursor();
+                        item.finished = item.loadError = true;
+                        mfp.updateStatus('error', mfp.st.ajax.tError.replace('%url%', item.src));
                     }
-                })
+                }, mfp.st.ajax.settings);
+
+                mfp.req = $.ajax(opts);
+
+                return '';
             }
+        }
+    });
+
+    /*>>ajax*/
+
+    /*>>image*/
+    var _imgInterval,
+        _getTitle = function(item) {
+            if (item.data && item.data.title !== undefined) {
+                return item.data.title;
+            }
+
+            var src = mfp.st.image.titleSrc;
+
+            if (src) {
+                if ($.isFunction(src)) {
+                    return src.call(mfp, item);
+                } else if (item.el) {
+                    return item.el.attr(src) || '';
+                }
+            }
+            return '';
+        };
+
+    $.magnificPopup.registerModule('image', {
+
+        options: {
+            markup: '<div class="mfp-figure">' +
+                        '<div class="mfp-close"></div>' +
+                        '<figure>' +
+                            '<div class="mfp-img"></div>' +
+                            '<figcaption>' +
+                                '<div class="mfp-bottom-bar">' +
+                                    '<div class="mfp-title"></div>' +
+                                    '<div class="mfp-counter"></div>' +
+                                '</div>' +
+                            '</figcaption>' +
+                        '</figure>' +
+                    '</div>',
+            cursor: 'mfp-zoom-out-cur',
+            titleSrc: 'title',
+            verticalFit: true,
+            tError: '<a href="%url%">The image</a> could not be loaded.'
+        },
+
+        proto: {
+            initImage: function() {
+                var imgSt = mfp.st.image,
+                    ns = '.image';
+
+                mfp.types.push('image');
+
+                _mfpOn(OPEN_EVENT + ns, function() {
+                    if (mfp.currItem.type === 'image' && imgSt.cursor) {
+                        $(document.body).addClass(imgSt.cursor);
+                    }
+                });
+
+                _mfpOn(CLOSE_EVENT + ns, function() {
+                    if (mfp.cursor) {
+                        $(document.body).removeClass(imgSt.cursor);
+                    }
+                    _window.off('resize' + EVENT_NS);
+                });
+
+                _mfpOn('Resize' + ns, mfp.resizeImage);
+                if (mfp.isLowIE) {
+                    _mfpOn('AfterChange', mfp.resizeImage);
+                }
+            },
+            resizeImage: function() {
+                var item = mfp.currItem;
+                if (!item || !item.img) return;
+
+                if (mfp.st.image.verticalFit) {
+                    var decr = 0;
+                    // fix box-sizing in ie7/8
+                    if (mfp.isLowIE) {
+                        decr = parseInt(item.img.css('padding-top'), 10) + parseInt(item.img.css('padding-bottom'), 10);
+                    }
+                    item.img.css('max-height', mfp.wH-decr);
+                }
+            },
+            _onImageHasSize: function(item) {
+                if (item.img) {
+
+                    item.hasSize = true;
+
+                    if (_imgInterval) {
+                        clearInterval(_imgInterval);
+                    }
+
+                    item.isCheckingImgSize = false;
+
+                    _mfpTrigger('ImageHasSize', item);
+
+                    if (item.imgHidden) {
+                        if (mfp.content) {
+                            mfp.content.removeClass('mfp-loading');
+                        }
+                        item.imgHidden = false;
+                    }
+                }
+            },
+
+            /**
+             * Function that loops until the image has size to display elements that rely on it asap
+             */
+            findImageSize: function(item) {
+
+                var counter = 0,
+                    img = item.img[0],
+                    mfpSetInterval = function(delay) {
+
+                        if (_imgInterval) {
+                            clearInterval(_imgInterval);
+                        }
+                        // decelerating interval that checks for size of an image
+                        _imgInterval = setInterval(function() {
+                            if (img.naturalWidth > 0) {
+                                mfp._onImageHasSize(item);
+                                return;
+                            }
+
+                            if (counter > 200) {
+                                clearInterval(_imgInterval);
+                            }
+
+                            counter++;
+                            if (counter === 3) {
+                                mfpSetInterval(10);
+                            } else if (counter === 40) {
+                                mfpSetInterval(50);
+                            } else if (counter === 100) {
+                                mfpSetInterval(500);
+                            }
+                        }, delay);
+                    };
+
+                mfpSetInterval(1);
+            },
+
+            getImage: function(item, template) {
+
+                var guard = 0,
+
+                    // image load complete handler
+                    onLoadComplete = function() {
+                        if (item) {
+                            if (item.img[0].complete) {
+                                item.img.off('.mfploader');
+
+                                if (item === mfp.currItem) {
+                                    mfp._onImageHasSize(item);
+
+                                    mfp.updateStatus('ready');
+                                }
+
+                                item.hasSize = true;
+                                item.loaded = true;
+
+                                _mfpTrigger('ImageLoadComplete');
+
+                            } else {
+                                // if image complete check fails 200 times (20 sec), we assume that there was an error
+                                guard++;
+                                if (guard < 200) {
+                                    setTimeout(onLoadComplete, 100);
+                                } else {
+                                    onLoadError();
+                                }
+                            }
+                        }
+                    },
+
+                    // image error handler
+                    onLoadError = function() {
+                        if (item) {
+                            item.img.off('.mfploader');
+                            if (item === mfp.currItem) {
+                                mfp._onImageHasSize(item);
+                                mfp.updateStatus('error', imgSt.tError.replace('%url', item.src));
+                            }
+
+                            item.hasSize = true;
+                            item.loaded = true;
+                            item.loadError = true;
+                        }
+                    },
+                    imgSt = mfp.st.image;
+
+                var el = template.find('.mfp-img');
+                if (el.length) {
+                    var img = document.createElement('img');
+                    img.className = 'mfp-img';
+                    if (item.el && item.el.find('img').length) {
+                        img.alt = item.el.find('img').attr('alt');
+                    }
+                    item.img = $(img).on('load.mfploader', onLoadComplete).on('error.mfploader', onLoadError);
+                    img.src = item.src;
+
+                    // without clone() "error" event is not firing when IMG is replaced by new IMG
+                    // TODO: find a way to avoid such cloning
+                    if (el.is('img')) {
+                        item.img = item.img.clone();
+                    }
+
+                    img = item.img[0];
+                    if (img.naturalWidth > 0) {
+                        item.hasSize = true;
+                    } else if (!img.width) {
+                        item.hasSize = false;
+                    }
+                }
+
+                mfp._parseMarkup(template, {
+                    title: _getTitle(item),
+                    img_replaceWidth: item.img
+                }, item);
+
+                mfp.resizeImage();
+
+                if (item.hasSize) {
+                    if (_imgInterval) {
+                        clearInterval(_imgInterval);
+                    }
+
+                    if (item.loadError) {
+                        template.addClass('mfp-loading');
+                        mfp.updateStatus('error', imgSt.tError.replace('%url%', item.src));
+                    } else {
+                        template.removeClass('mfp-loading');
+                        mfp.updateStatus('ready');
+                    }
+                    return template;
+                }
+
+                mfp.updateStatus('loading');
+                item.loading = true;
+
+                if (!item.hasSize) {
+                    item.imgHidden = true;
+                    template.addClass('mfp-loading');
+                    mfp.findImageSize(item);
+                }
+
+                return template;
+            }
+        }
+    });
+
+    /*>>image*/
+
+    /*>>zoom*/
+    var hasMozTransform,
+        getHasMozTransform = function() {
+            if (hasMozTransform === undefined) {
+                hasMozTransform = document.createElement('p').style.MozTransform !== undefined;
+            }
+            return hasMozTransform;
+        };
+
+    $.magnificPopup.registerModule('zoom', {
+
+        options: {
+            enabled: false,
+            easing: 'ease-in-out',
+            duration: 300,
+            opener: function(element) {
+                return element.is('img') ? element : element.find('img');
+            }
+        },
+
+        proto: {
+
+            initZoom: function() {
+                var zoomSt = mfp.st.zoom,
+                    ns = '.zoom',
+                    image;
+
+                if (!zoomSt.enabled || !mfp.supportsTransition) {
+                    return;
+                }
+
+                var duration = zoomSt.duration,
+                    getElToAnimate = function(image) {
+                        var newImg = image.clone().removeAttr('style').removeAttr('class').addClass('mfp-animated-image'),
+                            transition = 'all ' + (zoomSt.duration / 1000) + 's ' + zoomSt.easing,
+                            cssObj = {
+                                position: 'fixed',
+                                zIndex: 9999,
+                                left: 0,
+                                top: 0,
+                                '-webkit-backface-visibility': 'hidden'
+                            },
+                            t = 'transition';
+
+                        cssObj['-webkit-' + t] = cssObj['-moz-' + t] = cssObj['-o-' + t] = cssObj[t] = transition;
+
+                        newImg.css(cssObj);
+                        return newImg;
+                    },
+                    showMainContent = function() {
+                        mfp.content.css('visibility', 'visible');
+                    },
+                    openTimeout,
+                    animatedImg;
+
+                _mfpOn('BuildControls' + ns, function() {
+                    if (mfp._allowZoom()) {
+
+                        clearTimeout(openTimeout);
+                        mfp.content.css('visibility', 'hidden');
+
+                        // Basically, all the code below does is clone the existing image, and puts it on top of the current image and animates it
+
+                        image = mfp._getItemToZoom();
+
+                        if (!image) {
+                            showMainContent();
+                            return;
+                        }
+
+                        animatedImg = getElToAnimate(image);
+
+                        animatedImg.css(mfp._getOffset());
+
+                        mfp.wrap.append(animatedImg);
+
+                        openTimeout = setTimeout(function() {
+                            animatedImg.css(mfp._getOffset(true));
+                            openTimeout = setTimeout(function() {
+
+                                showMainContent();
+
+                                setTimeout(function() {
+                                    animatedImg.remove();
+                                    image = animatedImg = null;
+                                    _mfpTrigger('ZoomAnimationEnded');
+                                }, 16); // avoid blink when swtiching images
+
+                            }, duration); // this timeout equals animation duration
+
+                        }, 16); // by adding this timeout we avoid short glitch at the beginning of animation
+
+                        // Lots of timeouts...
+
+                    }
+                });
+                _mfpOn(BEFORE_CLOSE_EVENT + ns, function() {
+                    if (mfp._allowZoom()) {
+
+                        clearTimeout(openTimeout);
+
+                        mfp.st.removalDelay = duration;
+
+                        if (!image) {
+                            image = mfp._getItemToZoom();
+                            if (!image) {
+                                return;
+                            }
+                            animatedImg = getElToAnimate(image);
+                        }
+
+                        animatedImg.css(mfp._getOffset(true));
+                        mfp.wrap.append(animatedImg);
+                        mfp.content.css('visibility', 'hidden');
+
+                        setTimeout(function() {
+                            animatedImg.css(mfp._getOffset());
+                        }, 16);
+                    }
+
+                });
+
+                _mfpOn(CLOSE_EVENT + ns, function() {
+                    if (mfp._allowZoom()) {
+                        showMainContent();
+                        if (animatedImg) {
+                            animatedImg.remove();
+                        }
+                        image = null;
+                    }
+                });
+            },
+
+            _allowZoom: function() {
+                return mfp.currItem.type === 'image';
+            },
+
+            _getItemToZoom: function() {
+                if (mfp.currItem.hasSize) {
+                    return mfp.currItem.img;
+                } else {
+                    return false;
+                }
+            },
+
+            
         }
     })
 
