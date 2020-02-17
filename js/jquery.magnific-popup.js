3c22c57d3211b6a720af5dcb92b263a912a3bec4
AndrewSRea <andrew.s.rea@gmail.com>
2020-02-17 11:57:41 -0800

    Adding more scripts to magnific-popup

--- js/jquery.magnific-popup.js
+++ js/jquery.magnific-popup.js
@@ -869,5 +869,178 @@
         }
     };
 
-    
+    $.fn.magnificPopup = function(options) {
+        _checkInstance();
+
+        var jqEl = $(this);
+
+        // We call some API method if first param is a string
+        if (typeof options === 'string') {
+
+            if (options === 'open') {
+                var items,
+                    itemOpts = _isJQ ? jqEl.data('magnificPopup') : jqEl[0].magnificPopup,
+                    index = parseInt(arguments[1], 10) || 0;
+
+                if (itemOpts.items) {
+                    items = itemOpts.items[index];
+                } else {
+                    items = jqEl;
+                    if (itemOpts.delegate) {
+                        items = items.find(itemOpts.delegate);
+                    }
+                    items = items.eq(index);
+                }
+                mfp._openClick({ mfpEl: items }, jqEl, itemOpts);
+            } else {
+                if (mfp.isOpen) {
+                    mfp[options].apply(mfp, Array.prototype.slice.call(arguments, 1));
+                }
+            }
+        } else {
+            //clone options obj
+            options = $.extend(true, {}, options);
+
+            /**
+             * As Zepto doesn't support .data() method for objects
+             * and it works only in normal browsers
+             * we assign "options" object directly to the DOM element.
+             */
+            if (_isJQ) {
+                jqEl.data('magnificPopup', options);
+            } else {
+                jqEl[0].magnificPopup = options;
+            }
+
+            mfp.addGroup(jqEl, options);
+        }
+        return jqEl;
+    };
+
+    /*>>core*/
+
+    /*>>inline*/
+
+    var INLINE_NS = 'inline',
+        _hiddenClass,
+        _inlinePlaceholder,
+        _lastInlineElement,
+        _putInlineElementsBack = function() {
+            if (_lastInlineElement) {
+                _inlinePlaceholder.after(_lastInlineElement.addClass(_hiddenClass)).detach();
+                _lastInlineElement = null;
+            }
+        };
+
+    $.magnificPopup.registerModule(INLINE_NS, {
+        options: {
+            hiddenClass: 'hide', // will be appended with 'mfp-' prefix
+            markup: '',
+            tNotFound: 'Content not found'
+        },
+        proto: {
+
+            initInline: function() {
+                mfp.types.push(INLINE_NS);
+
+                _mfpOn(CLOSE_EVENT + '.' + INLINE_NS, function() {
+                    _putInlineElementsBack();
+                });
+            },
+
+            getInline: function(item, template) {
+
+                _putInlineElementsBack();
+
+                if (item.src) {
+                    var inlineSt = mfp.st.inline,
+                        el = $(item.src);
+
+                    if (el.length) {
+
+                        // If target element has parent - we replace it with placeholderand put it back after popup is closed
+                        var parent = el[0].parentNode;
+                        if (parent && parent.tagName) {
+                            if (!_inlinePlaceholder) {
+                                _hiddenClass = inlineSt.hiddenClass;
+                                _inlinePlaceholder = _getEl(_hiddenClass);
+                                _hiddenClass = 'mfp-' + _hiddenClass;
+                            }
+                            // replace target inline element with placeholder
+                            _lastInlineElement = el.after(_inlinePlaceholder).detach().removeClass(_hiddenClass);
+                        }
+
+                        mfp.updateStatus('ready');
+                    } else {
+                        mfp.updateStatus('error', inlineSt.tNotFound);
+                        el = $('<div>');
+                    }
+
+                    item.inlineElement = el;
+                    return el;
+                }
+
+                mfp._updateStatus('ready');
+                mfp._parseMarkup(template, {}, item);
+                return template;
+            }
+        }
+    });
+
+    /*>>inline*/
+
+    /*>>ajax*/
+    var AJAX_NS = 'ajax',
+        _ajaxCur,
+        _removeAjaxCursor = function() {
+            if (_ajaxCur) {
+                $(document.body).removeClass(_ajaxCur);
+            }
+        },
+        _destroyAjaxRequest = function() {
+            _removeAjaxCursor();
+            if (mfp.req) {
+                mfp.req.abort();
+            }
+        };
+
+    $.magnificPopup.registerModule(AJAX_NS, {
+
+        options: {
+            settings: null,
+            cursor: 'mfp-ajax-cur',
+            tError: '<a href="%url%">The content</a> could not be loaded.'
+        },
+
+        proto: {
+            initAjax: function() {
+                mfp.types.push(AJAX_NS);
+                _ajaxCur = mfp.st.ajax.cursor;
+
+                _mfpOn(CLOSE_EVENT + '.' + AJAX_NS, _destroyAjaxRequest);
+                _mfpOn('BeforeChange.' + AJAX_NS, _destroyAjaxRequest);
+            },
+            getAjax: function(item) {
+
+                if(_ajaxCur) {
+                    $(document.body).addClass(_ajaxCur);
+                }
+
+                mfp.updateStatus('loading');
+
+                var opts = $.extend({
+                    url: item.src,
+                    success: function(data, textStatus, jqXHR) {
+                        var temp = {
+                            data: data,
+                            xhr: jqXHR
+                        };
+
+                        _mfpTrigger('ParseAjax', temp);
+                    }
+                })
+            }
+        }
+    })
+
 }))
