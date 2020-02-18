c3704e339af8e5abd8646867348ddea993879038
AndrewSRea <andrew.s.rea@gmail.com>
2020-02-17 17:11:59 -0800

    Adding more scripts to magnific-popup

--- js/jquery.magnific-popup.js
+++ js/jquery.magnific-popup.js
@@ -1463,7 +1463,236 @@
                 }
             },
 
-            
+            // Get element position relative to viewport
+            _getOffset: function(isLarge) {
+                var el;
+                if (isLarge) {
+                    el = mfp.currItem.img;
+                } else {
+                    el = mfp.st.zoom.opener(mfp.currItem.el || mfp.currItem);
+                }
+
+                var offset = el.offset();
+                var paddingTop = parseInt(el.css('padding-top'), 10);
+                var paddingBottom = parseInt(el.css('padding-bottom'), 10);
+                offset.top -= ($(window).scrollTop() - paddingTop);
+
+                /**
+                 * Animating left + top + width/height looks glitchy in Firefox, but perfect in Chrome. And vice-versa.
+                 */
+                var obj = {
+                    width: el.Width(),
+                    // fix Zepto  height + padding issue
+                    height: (_isJQ ? el.innerHeight() : el[0].offsetHeight) - paddingBottom - paddingTop
+                };
+
+                if (getHasMozTransform()) {
+                    obj['-moz-transform'] = obj['transform'] = 'translate(' + offset.left + 'px,' + offset.top + 'px)';
+                } else {
+                    obj.left = offset.left;
+                    obj.top = offset.top;
+                }
+                return obj;
+            }
+        }
+    });
+
+    /*>>zoom*/
+
+    /*>>iframe*/
+
+    var IFRAME_NS = 'iframe',
+        _emptyPage = '//about:blank',
+
+        _fixIframeBugs = function(iShowing) {
+            if (mfp.currTemplate[IFRAME_NS]) {
+                var el = mfp.currTemplate[IFRAME_NS].find('iframe');
+                if (el.length) {
+                    // reset src after the popup is closed to avoid "video keeps playing after popup is closed" bug
+                    if (!isShowing) {
+                        el[0].src = _emptyPage;
+                    }
+
+                    // IE8 black screen bug fix
+                    if (mfp.isIE8) {
+                        el.css('display', isShowing ? 'block' : 'none');
+                    }
+                }
+            }
+        };
+
+    $.magnificPopup.registerModule(IFRAME_NS, {
+
+        options: {
+            markup: '<div class="mfp-iframe-scaler">' +
+                        '<div class="mfp-close"></div>' +
+                        '<iframe class="mfp-iframe" src="//about:blank" frameborder="0" allowfullscreen></iframe>' +
+                    '</div>',
+
+            srcAction: 'iframe_src',
+
+            // we support only one default type of URL by default
+            patterns: {
+                youtube: {
+                    index: 'youtube.com',
+                    id: 'v=',
+                    src: '//www.youtube.com/embed/%id%?autoplay=1'
+                },
+                vimeo: {
+                    index: 'vimeo.com/',
+                    id: '/',
+                    src: '//player.vimeo.com/video/%id%?autoplay=1'
+                },
+                gmaps: {
+                    index: '//maps.google.',
+                    src: '%id%&output=embed'
+                }
+            }
+        },
+
+        proto: {
+            initIframe: function() {
+                mfp.types.push(IFRAME_NS);
+
+                _mfpOn('BeforeChange', function(e, prevType, newType) {
+                    if (prevType === IFRAME_NS) {
+                        _fixIframeBugs(); // iframe if removed
+                    } else if (newType === IFRAME_NS) {
+                        _fixIframeBugs(true); // iframe is showing
+                    }
+                });
+
+                _mfpOn(CLOSE_EVENT + '.' + IFRAME_NS, function() {
+                    _fixIframeBugs();
+                });
+            },
+
+            getIframe: function(item, template) {
+                var embedSrc = item.src;
+                var iframeSt = mfp.st.iframe;
+
+                $.each(iframeSt.patterns, function() {
+                    if (embedSrc.indexOf(this.index) > -1) {
+                        if (this.id) {
+                            if (typeof this.id === 'string') {
+                                embedSrc = embedSrc.substr(embedSrc.lastIndexOf(this.id) + this.id.length, embedSrc.length);
+                            } else {
+                                embedSrc = this.id.call(this, embedSrc);
+                            }
+                        }
+                        embedSrc = this.src.replace('%id', embedSrc);
+                        return false;
+                    }
+                });
+
+                var dataObj = {};
+                if (iframeSt.srcAction) {
+                    dataObj[iframeSt.srcAction] = embedSrc;
+                }
+                mfp._parseMarkup(template, dataObj, item);
+
+                mfp.updateStatus('ready');
+
+                return template;
+            }
+        }
+    });
+
+    /*>>iframe*/
+
+    /*>>gallery*/
+    /**
+     * Get looped index depending on number of slides
+     */
+    var _getLoopedId = function(index) {
+            var numSlides = mfp.items.length;
+            if (index > numSlides - 1) {
+                return index - numSlides;
+            } else if (index < 0) {
+                return numSlides + index;
+            }
+            return index;
+        },
+        _replaceCurrTotal = function(text, curr, total) {
+            return text.replace(/%curr%/gi, curr + 1).replace(/%total%/gi, total);
+        };
+
+    $.magnificPopup.registerModule('gallery', {
+
+        options: {
+            enabled: false,
+            arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>',
+            preload: [0,2],
+            navigateByImgClick: true,
+            arrows: true,
+
+            tPrev: 'Previous (Left arrow key)',
+            tNext: 'Next (Right arrow key)',
+            tCounter: '%curr% of %total%'
+        },
+
+        proto: {
+            initGallery: function() {
+
+                var gSt = mfp.st.gallery,
+                    ns = '.mfp-gallery';
+
+                mfp.direction = true; // true - next, false - prev
+
+                if (!gSt || !gSt.enabled) {
+                    return false;
+                }
+
+                _wrapClasses += ' mfp-gallery';
+
+                _mfpOn(OPEN_EVENT + ns, function() {
+
+                    if (gSt.navigateByImgClick) {
+                        mfp.wrap.on('click' + ns, '.mfp-img', function() {
+                            if (mfp.items.length > 1) {
+                                mfp.next();
+                                return false;
+                            }
+                        });
+                    }
+
+                    _document.on('keydown' + ns, function() {
+                        if (e.keyCode === 37) {
+                            mfp.prev();
+                        } else if (e.keyCode === 39) {
+                            mfp.next();
+                        }
+                    });
+                });
+
+                _mfpOn('UpdateStatus' + ns, function(e, data) {
+                    if (data.text) {
+                        data.text = _replaceCurrTotal(data.text, mfp.currItem.index, mfp.items.length);
+                    }
+                });
+
+                _mfpOn(MARKUP_PARSE_EVENT + ns, function(e, element, values, item) {
+                    var l = mfp.items.length;
+                    values.counter = l > 1 ? _replaceCurrTotal(gSt.tCounter, item.index, l) : '';
+                });
+
+                _mfpOn('BuildControls' + ns, function() {
+                    if (mfp.items.length > 1 && gSt.arrows && !mfp.arrowLeft) {
+                        var markup = gSt.arrowMarkup,
+                            arrowLeft = mfp.arrowLeft = $(markup.replace(/%title%/gi, gSt.tPrev).replace(/%dir%/gi, 'left')).addClass(PREVENT_CLOSE_CLASS),
+                            arrowRight = mfp.arrowRight = $(markup.replace(/%title%/gi, gSt.tNext).replace(/%dir%/gi, 'right')).addClass(PREVENT_CLOSE_CLASS);
+
+                        arrowLeft.click(function() {
+                            mfp.prev();
+                        });
+                        arrowRight.click(function() {
+                            mfp.next();
+                        });
+
+                        
+                    }
+                })
+            }
         }
     })
 
