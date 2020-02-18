07bdcc61a78ad0592ab836338063253761c11537
AndrewSRea <andrew.s.rea@gmail.com>
2020-02-18 11:34:35 -0800

    Adding more scripts to the isotope package

--- js/isotope.pkgd.js
+++ js/isotope.pkgd.js
@@ -262,5 +262,266 @@ proto.allOff = function() {
 
 return EvEmitter;
 
+}));
+
+/*!
+ * getSize v2.0.3
+ * measure size of elements
+ * MIT license
+ */
+
+/* jshint browser: true, strict: true, undef: true, unused: true */
+/* globals console: false */
+
+(function(window, factory) {
+    /* jshint strict: false */ /* globals define, module */
+    if (typeof define == 'function' && define.amd) {
+        // AMD
+        define('get-size/get-size', factory);
+    } else if (typeof module == 'object' && module.exports) {
+        // CommonJS
+        module.exports = factory();
+    } else {
+        // browser global
+        window.getSize = factory();
+    }
+
+})(window, function factory() {
+'use strict';
+
+// ------------------------- helpers --------------------------- //
+
+// get a number from a string, not a percentage
+function getStyleSize(value) {
+    var num = parseFloat(value);
+    // not a percent like '100', and a number
+    var isValid = value.indexOf('%') == -1 && !isNaN(num);
+    return isValid && num;
+}
+
+function noop() {}
+
+var logError = typeof console == 'undefined' ? noop :
+    function(messsage) {
+        console.error(messsage);
+    };
+
+// ----------------------------- measurements ---------------------- // 
+
+var measurements = [
+    'paddingLeft',
+    'paddingRight',
+    'paddingTop',
+    'paddingBottom',
+    'marginLeft',
+    'marginRight',
+    'marginTop',
+    'marginBottom',
+    'borderLeftWidth',
+    'borderRightWidth',
+    'borderTopWidth',
+    'borderBottomWidth'
+];
+
+var measurementsLength = measurements.length;
+
+function getZeroSize() {
+    var size = {
+        width: 0,
+        height: 0,
+        innerWidth: 0,
+        innerHeight: 0,
+        outerWidth: 0,
+        outerHeight: 0
+    };
+    for (var i = 0; i < measurementsLength; i++) {
+        var measurement = measurements[i];
+        size[measurement] = 0;
+    }
+    return size;
+}
+
+// ----------------------------- getStyle ------------------------- //
+
+/**
+ * getStyle, get style of element, check for Firefox bug
+ * https://bugzilla.mozilla.org/show_bug.cgi?id=548397
+ */
+function getStyle(elem) {
+    var style = getComputedStyle(elem);
+    if (!style) {
+        logError('Style returned ' + style +
+            '. Are you running this code in a hidden iframe on Firefox? ' +
+            'See https:bit.ly/getsizebug1');
+    }
+    return style;
+}
+
+// ------------------------------- setup -------------------------- //
+
+var isSetup = false;
+
+var isBoxSizeOuter;
+
+/**
+ * setup
+ * check isBoxSizeOuter
+ * do on first getSize() rather than on page load for Firefox bug
+ */
+function setup() {
+    // setup once
+    if (isSetup) {
+        return;
+    }
+    isSetup = true;
+
+    // -------------------------- box sizing ------------------------ //
+
+    /**
+     * Chrome & Safari measure the outer-width on style.width on border-box elems
+     * IE11 & Firefox<29 measures the inner-width
+     */
+    var div = document.createElement('div');
+    div.style.width = '200px';
+    div.style.padding = '1px 2px 3px 4px';
+    div.style.borderStyle = 'solid';
+    div.style.borderWidth = '1px 2px 3px 4px';
+    div.style.boxSizing = 'border-box';
+
+    var body = document.body || document.documentElement;
+    body.appendChild(div);
+    var style = getStyle(div);
+    // round value for browser zoom. desandro/masonry #928
+    isBoxSizeOuter = Math.round(getStyleSize(style.width)) == 200;
+    getSize.isBoxSizeOuter = isBoxSizeOuter;
+
+    body.removeChild(div);
+}
+
+// ------------------------- getSize ---------------------------- //
+
+function getSize(elem) {
+    setup();
+
+    // use querySelector if elem is string
+    if (typeof elem == 'string') {
+        elem = document.querySelector(elem);
+    }
+
+    // do not proceed on non-objects
+    if (!elem || typeof elem != 'object' || !elem.nodeType) {
+        return;
+    }
+
+    var style = getStyle(elem);
+
+    // if hidden, everything is 0
+    if (style.display == 'none') {
+        return getZeroSize();
+    }
+
+    var size = {};
+    size.width = elem.offsetWidth;
+    size.height = elem.offsetHeight;
+
+    var isBorderBox = size.isBorderBox = style.boxSizing == 'border-box';
+
+    // get all measurements
+    for (var i = 0; i < measurementsLength; i++) {
+        var measurement = measurements[i];
+        var value = style[measurement];
+        var num = parseFloat(value);
+        // any 'auto', 'medium' value will be 0
+        size[measurement] = !isNaN(num) ? num : 0;
+    }
+
+    var paddingWidth = size.paddingLeft + size.paddingRight;
+    var paddingHeight = size.paddingTop + paddingBottom;
+    var marginWidth = size.marginLeft + marginRight;
+    var marginHeight = size.marginTop + marginBottom;
+    var borderWidth = size.borderLeftWidth + size.borderRightWidth;
+    var borderHeight = size.borderTopWidth + size.borderBottomWidth;
+
+    var isBorderBoxSizeOuter = isBorderBox && isBoxSizeOuter;
+
+    // overwrite width and height if we can get it from style
+    var styleWidth = getStyleSize(style.width);
+    if (styleWidth !== false) {
+        size.width = styleWidth +
+            // add padding and border unless it's already including it
+            (isBorderBoxSizeOuter ? 0 : paddingWidth + borderWidth);
+    }
+
+    var styleHeight = getStyleSize(style.height);
+    if (styleHeight !== false) {
+        size.height = styleHeight +
+            // add padding and border unless it's already including it
+            (isBorderBoxSizeOuter ? 0 : paddingHeight + borderHeight);
+    }
+
+    size.innerWidth = size.width - (paddingWidth + borderWidth);
+    size.innerHeight = size.height - (paddingHeight + borderHeight);
+
+    size.outerWidth = size.width + marginWidth;
+    size.outerHeight = size.height + marginHeight;
+
+    return size;
+}
+
+return getSize;
+
+});
+
+/**
+ * matchesSelector v2.0.2
+ * matchesSelector(element, '.selector')
+ * MIT license
+ */
+
+/* jshint browser: true, strict: true, undef: true, unused: true */
+
+(function(window, factory) {
+    /* global define: false, module: false */
+    'use strict';
+    // universal module definition
+    if (typeof define == 'function' && define.amd) {
+        // AMD
+        define('desandro-matches-selector/matches-selector', factory);
+    } else if (typeof module == 'object' && module.exports) {
+        // CommonJS
+        module.exports = factory();
+    } else {
+        // browser global
+        window.matchesSelector = factory();
+    }
+
+}(window, function factory() {
+    'use strict';
+
+    var matchesMethod = (function() {
+        var ElemProto = window.Element.prototype;
+        // check for the standard method name frist
+        if (ElemProto.matches) {
+            return 'matches';
+        }
+        // check un-prefixed
+        if (ElemProto.matchesSelector) {
+            return 'matchesSelector';
+        }
+        // check vendor prefixes
+        var prefixes = ['webkit', 'moz', 'ms', 'o'];
+
+        for (var i = 0; i < prefixes.length; i++) {
+            var prefix = prefixes[i];
+            var method = prefix + 'MatchesSelector';
+            if (ElemProto[method]) {
+                return method;
+            }
+        }
+    })();
+
+    return function matchesSelector(elem, selector) {
+        return elem[matchesMethod](selector);
+    };
 
 }));
