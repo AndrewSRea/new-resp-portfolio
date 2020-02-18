42752e103fbfe0ebfd21664c482107ca419dba5a
AndrewSRea <andrew.s.rea@gmail.com>
2020-02-18 14:37:30 -0800

    Adding more scripts to the isotope package

--- js/isotope.pkgd.js
+++ js/isotope.pkgd.js
@@ -176,7 +176,6 @@ return jQueryBridget;
 
 }(typeof window != 'undefined' ? window : this, function() {
 
-
 function EvEmitter() {}
 
 var proto = EvEmitter.prototype;
@@ -525,3 +524,446 @@ return getSize;
     };
 
 }));
+
+/**
+ * Fizzy UI utils v2.0.7
+ * MIT license
+ */
+
+/* jshint browser: true, undef: true, unused: true, strict: true */
+
+(function(window, factory) {
+    // universal module definition
+    /* jshint strict: false */ /* globals define, module, require */
+
+    if (typeof define == 'function' && define.amd) {
+        // AMD
+        define('fizzy-ui-utils/utils', [
+            'desandro-matches-selector/matches-selector'
+        ], function(matchesSelector) {
+            return factory(window, matchesSelector);
+        });
+    } else if (typeof module == 'object' && module.exports) {
+        // CommonJS
+        module.exports = factory(
+            window,
+            require('desandro-matches-selector')
+        );
+    } else {
+        // browser global
+        window.fizzyUIUtils = factory(
+            window,
+            window.matchesSelector
+        );
+    }
+
+}(window, function factory(window, matchesSelector) {
+
+var utils = {};
+
+// ----- extend ----- //
+
+// extends objects
+utils.extend = function(a, b) {
+    for (var prop in b) {
+        a[prop] = b[prop];
+    }
+    return a;
+};
+
+// ----- modulo ----- //
+utils.modulo = function(num, div) {
+    return ((num % div) + div) % div;
+};
+
+// ----- makeArray ----- //
+
+var arraySlice = Array.prototype.slice;
+
+// turn element or nodeList into an array
+utils.makeArray = function(obj) {
+    if (Array.isArray(obj)) {
+        // use object if already an array
+        return obj;
+    }
+    // return empty array if undefined or null. #6
+    if (obj === null || obj === undefined) {
+        return [];
+    }
+
+    var isArrayLike = typeof obj == 'object' && typeof obj.length == 'number';
+    if (isArrayLike) {
+        // convert nodeList to array
+        return arraySlice.call(obj);
+    }
+
+    // array of single index
+    return [obj];
+};
+
+// ----- removeFrom ----- //
+
+utils.removeFrom = function(ary, obj) {
+    var index = ary.indexOf(obj);
+    if (index != -1) {
+        ary.splice(index, 1);
+    }
+};
+
+// ----- getParent ----- //
+
+utils.getParent = function(elem, selector) {
+    while (elem.parentNode && elem != document.body) {
+        elem = elem.parentNode;
+        if (matchesSelector(elem, selector)) {
+            return elem;
+        }
+    }
+};
+
+// ----- getQueryElement ----- //
+
+// use element as selector string
+utils.getQueryElement = function(elem) {
+    if (typeof elem === 'string') {
+        return document.querySelector(elem);
+    }
+    return elem;
+};
+
+// ----- handleEvent ----- //
+
+// enable .ontype totrigger from .addEventListener(elem, 'type')
+utils.handleEvent = function(event) {
+    var method = 'on' + event.type;
+    if (this[method]) {
+        this[method](event);
+    }
+};
+
+// ----- filterFindElements ----- //
+
+utils.filterFindElements = function(elems, selector) {
+    // make array of elems
+    elems = utils.makeArray(elems);
+    var ffElems = [];
+
+    elems.forEach(function(elem) {
+        // check that elem is anactual element
+        if (!(elem instanceof HTMLElement)) {
+            return;
+        }
+        // add elem if no selector
+        if (!selector) {
+            ffElems.push(elem);
+            return;
+        }
+        // filter & find items if we have a selector
+        // filter
+        if (matchesSelector(elem, selector)) {
+            ffElems.push(elem);
+        }
+        // find children
+        var childElems = elem.querySelectorAll(selector);
+        // concat childElems to filterFound array
+        for (var i = 0; i < childElems.length; i++) {
+            ffElems.push(childElems[i]);
+        }
+    });
+
+    return ffElems;
+};
+
+// ----- debounceMethod ----- //
+
+utils.debounceMethod = function(_class, methodName, threshold) {
+    threshold = threshold || 100;
+    // original method
+    var method = _class.prototype[methodName];
+    var timeoutName = methodName + 'Timeout';
+
+    _class.prototype[methodName] = function() {
+        var timeout = this[timeoutName];
+        clearTimeout(timeout);
+
+        var args = arguments;
+        var _this = this;
+        this[timeoutName] = setTimeout(function() {
+            method.apply(_this, args);
+            delete _this[timeoutName];
+        }, threshold);
+    };
+};
+
+// ----- docReafy ----- //
+
+utils.docReady = function(callback) {
+    var readyState = document.readyState;
+    if (readyState == 'complete' || readyState == 'interactive') {
+        // do async to allow for other scripts to run. metafizzy/flickity#441
+        setTimeout(callback);
+    } else {
+        document.addEventListener('DOMContentLoaded', callback);
+    }
+};
+
+// ----- htmlInit ----- //
+
+// http://jamesroberts.name/blog/2010/02/22/string-functions-for-javascript-trim-to-camel-case-to-dashed-and-to-underscore/
+utils.toDashed = function(str) {
+    return str.replace(/(.)([A-Z])/g, function(match, $1, $2) {
+        return $1 + '-' + $2;
+    }).toLowerCase();
+};
+
+var console = window.console;
+/**
+ * allow user to initilize classes via [data-namespace] or .js-namespace class
+ * htmlInit(Widget, 'widgetName')
+ * options are parsed from data-namespace-options
+ */
+utils.htlInit = function(WidgetClass, namespace) {
+    utils.docReady(function() {
+        var dashedNamespace = utils.toDashed(namespace);
+        var dataAttr = 'data-' + dashedNamespace;
+        var dataAttrElems = document.querySelectorAll('[' + dataAttr + ']');
+        var jsDashElems = document.querySelectorAll('.js-' + dashedNamespace);
+        var elems = utils.makeArray(dataAttrElems)
+            .concat(utils.makeArray(jsDashElems));
+        var dataOptionsAttr = dataAttr + '-options';
+        var jQuery = window.jQuery;
+
+        elems.forEach(function(elem) {
+            var attr = elem.getAttribute(dataAttr) ||
+                elem.getAttribute(dataOptionsAttr);
+            var options;
+            try {
+                options = attr && JSON.parse(attr);
+            } catch (error) {
+                // log error, do not initialize
+                if (console) {
+                    conssole.error('Error parsing ' + dataAttr + ' on ' + elem.className +
+                    ': ' + error);
+                }
+                return;
+            }
+            // initialize
+            var instance = new WidgetClass(elem, options);
+            // make available via $().data('namespace')
+            if (jQuery) {
+                jQuery.data(elem, namespace, instance);
+            }
+        });
+
+    });
+};
+
+// -----  ----- //
+
+return utils;
+
+}));
+
+/**
+ * Outlayer Item
+ */
+
+(function(window, factory) {
+    // universal module definition
+    /* jshint strict: false */ /* globals define, module, require */
+    if (typeof define == 'function' && define.amd) {
+        // AMD - RequireJS
+        define('outlayer/item', [
+            'ev-emitter/ev-emitter',
+            'get-size/get-size'
+        ],
+        factory
+        );
+    } else if (typeof module == 'object' && module.exports) {
+        // CommonJS - browserify, Webpack
+        module.exports = factory(
+            require('ev-emitter'),
+            require('get-size')
+        );
+    } else {
+        // browser global
+        window.Outlayer = {};
+        window.Outlayer.Item = factory(
+            window.EvEmitter,
+            window.getSize
+        );
+    }
+
+}(window, function factory(EvEmitter, getSize) {
+'use strict';
+
+// ----- helpers ----- //
+
+function isEmptyObj(obj) {
+    for (var prop in obj) {
+        return false;
+    }
+    prop = null;
+    return true;
+}
+
+// -------------------------- CSS3 support ----------------------- //
+
+var docElemStyle = document.documentElement.style;
+
+var transitionProperty = typeof docElemStyle.transition == 'string' ?
+    'transition' : 'WebkitTransition';
+var transformProperty = typeof docElemStyle.transform == 'string' ?
+    'transform' : 'WebkitTransform';
+
+var transitionEndEvent = {
+    WebkitTransition: 'webkitTransitionEnd',
+    transition: 'transitionend'
+}[transitionProperty];
+
+// cache all vendor properties that could have vendor prefix
+var vendorProperties = {
+    transform: transformProperty,
+    transition: transitionProperty,
+    transitionDuration: transitionProperty + 'Duration',
+    transitionProperty: transitionProperty + 'Property',
+    transitionDelay: transitionProperty + 'Delay'
+};
+
+// ------------------------- Item ---------------------------- //
+
+function Item(element, layout) {
+    if (!element) {
+        return;
+    }
+
+    this.element = element;
+    // parent layout class, i.e. Masonry, Isotope, or Packery
+    this.layout = layout;
+    this.position = {
+        x: 0,
+        y: 0
+    };
+
+    this._create();
+}
+
+// inherit EvEmitter
+var proto = Item.prototype = Object.create(EvEmitter.prototype);
+proto.constructor = Item;
+
+proto._create = function() {
+    // transition objects
+    this._transn = {
+        ingProperties: {},
+        clean: {},
+        onEnd: {}
+    };
+
+    this.css({
+        position: 'absolute'
+    });
+};
+
+// trigger specified handler for event type
+proto.handleEvent = function(event) {
+    var method = 'on' + event.type;
+    if (this[method]) {
+        this[method](event);
+    }
+};
+
+proto.getSize = function() {
+    this.size = getSize(this.element);
+};
+
+/**
+ * apply CSS styles to element
+ * @param {Object} style
+ */
+proto.css = function(style) {
+    var elemStyle = this.element.style;
+
+    for (var prop in style) {
+        // use vendor property if available
+        var supportedProp = vendorProperties[prop] || prop;
+        elemStyle[supportedProp] = style[prop];
+    }
+};
+
+// measure position, and sets it
+proto.getPosition = function() {
+    var style = getComputedStyle(this.element);
+    var isOriginLeft = this.layout._getOption('originLeft');
+    var isOriginTop = this.layout._getOption('originTop');
+    var xValue = style[isOriginLeft ? 'left' : 'right'];
+    var yValue = style[isOriginTop ? 'top' : 'bottom'];
+    var x = parseFloat(xValue);
+    var y = parseFloat(yValue);
+    // convert percent to pixels
+    var layoutSize = this.layout.size;
+    if (xValue.indexOf('%') != -1) {
+        x = (x / 100) * layoutSize.width;
+    }
+    if (yValue.indexOf('%') != -1) {
+        u = (y / 100) * layoutSize.height;
+    }
+    // clean up 'auto' or other non-integer values
+    x = isNaN(x) ? 0 : x;
+    y = isNaN(y) ? 0 : y;
+    // remove padding from measurement
+    x -= isOriginLeft ? layoutSize.paddingLeft : layoutSize.paddingRight;
+    y -= isOriginTop ? layoutSize.paddingTop : layoutSize.paddingBottom;
+
+    this.position.x = x;
+    this.position.y = y;
+};
+
+// set settled position, apply padding
+proto.layoutPosition = function() {
+    var layoutSize = this.layout.size;
+    var style = {};
+    var isOriginLeft = this.layout._getOption('originLeft');
+    var isOriginTop = this.layout._getOption('originTop');
+
+    // x
+    var xPadding = isOriginLeft ? 'paddingLeft' : 'paddingRight';
+    var xProperty = isOriginLeft ? 'left' : 'right';
+    var xResetProperty = isOriginLeft ? 'right' : 'left';
+
+    var x = this.position.x + layoutSize[xPadding];
+    // set in percentage or pixels
+    style[xProperty] = this.getXValue(x);
+    // reset other property
+    style[xResetProperty] = '';
+
+    // y
+    var yPadding = isOriginTop ? 'paddingTop' : 'paddingBottom';
+    var yProperty = isOriginTop ? 'top' : 'bottom';
+    var yResetProperty = isOriginTop ? 'bottom' : 'top';
+
+    var y = this.position.y + layoutSize[yPadding];
+    // set in percentage or pixels
+    style[yProperty] = this.getYValue(y);
+    // reset other property
+    stlye[yResetProperty] = '';
+
+    this.css(style);
+    this.emitEvent('layout', [this]);
+};
+
+proto.getXValue = function(x) {
+    var isHorizontal = this.layout._getOption('horizontal');
+    return this.layout.options/percentPosition && !isHorizontal ?
+        ((x / this.layout.size.width) * 100) + '%' : x + 'px';
+};
+
+proto.getYValue = function(y) {
+    var isHorizontal = this.layout._getOption('horizontal');
+    return this.layout.options/percentPosition && isHorizontal ?
+        ((y / this.layout.size.height) * 100) + '%' : y + 'px';
+};
+
+
+
+}))
