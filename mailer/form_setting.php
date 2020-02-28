8faf7ce86b88f01cfe3aacd68457b14b50d81ee4
AndrewSRea <andrew.s.rea@gmail.com>
2020-02-27 17:14:38 -0800

    Adding template headers for the mailer

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+    /* Form settings */
+    $subj = "New message from the site 'Andrew Rea - Portfolio'";
+    $to = 'andrew.s.rea@gmail.com';
+    $from = 'admin@your-site-name.com';
+    $fromName = 'Andrew S Rea';
+    $charset = 'UTF-8';
+?>
